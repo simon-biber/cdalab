06018899b0e8ede6e72c5998b9651b4c676ea7c9
Simon Biber <simon@srkb.net>
2020-04-22 19:20:51 +0930

    Initial Commit

---
+++ cdalab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cdalab
+//
+//  Created by Simon Biber on 22/4/20.
+//  Copyright © 2020 Simon Biber. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
